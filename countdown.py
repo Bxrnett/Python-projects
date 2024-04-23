60d547dd8edc6b52fbf19f617acfa54bf422de66
Will Barnett <149579049+Bxrnett@users.noreply.github.com>
2024-04-23 08:38:17 +0100

    Countdown App
    Been continuing my learning of python by making some more simple CMD apps to get used to the syntax, here is a Countdown app I have made

---
+++ countdown.py
@@ -0,0 +1,22 @@
+import datetime
+def calculate_countdown(target_date):
+ current_date = datetime.datetime.now()
+ time_left = target_date - current_date
+ return time_left
+
+# Get the target date from the user
+target_date_str = input("Enter the target date (DD-MM-YYYY): ")
+target_date = datetime.datetime.strptime(target_date_str, "%d-%m-%Y")
+
+# Calculate the countdown
+countdown = calculate_countdown(target_date)
+
+# Display the countdown
+days = countdown.days
+hours, remainder = divmod(countdown.seconds, 3600)
+minutes, seconds = divmod(remainder, 60)
+print("Countdown:")
+print("Days:", days)
+print("Hours:", hours)
+print("Minutes:", minutes)
+print("Seconds:", seconds)
